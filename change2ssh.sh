830492b772722fc462802ccc1c86db128e49fbf5
Gary B. Corell <wa5qjh@xmission.com>
2017-04-30 08:47:45 +0800

    modified:   change2ssh.sh

--- change2ssh.sh
+++ change2ssh.sh
@@ -4,10 +4,10 @@
 # 04/30/17  08:24 AM
 
 echo "Change to ssh mode for github"
-echo, "This will take a few minutes. We are setting up an ssh key on  github"
+echo "This will take a few minutes. We are setting up an ssh key on  github"
 ssh -T git@github.com
 #
-echo, " next we change the urls"
+echo " next we change the urls"
 git remote set-url origin git@github.com:wa5qjh/Perl.git
 echo " then verify it was set properly.  you should see something like:"
 echo "origin git@github.com:wa5qjh/Perl.git (fetch)"
@@ -22,5 +22,4 @@ git remote -v
 
 # just incase it's needed, heres a command to set the upstream urls
 # git push --set-upstream git@github.com:wa5qjh/Perl.git master
-To revert:  git push --set-upstream https://github.com/wa5qjh/Perl.git master
-
+#To revert:  git push --set-upstream https://github.com/wa5qjh/Perl.git master
