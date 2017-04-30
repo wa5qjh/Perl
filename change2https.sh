007708cca469fb42390880d71fc95d5f71727e7a
Gary B. Corell <wa5qjh@xmission.com>
2017-04-30 08:52:09 +0800

    modified:   change2https.sh

--- change2https.sh
+++ change2https.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Gary Corell
 # 04/30/17  07:44 AM
+#04/30/17  08:49 AM
 
 echo "Change to HTTPS  mode for github"
-echo "checking the current URL's. If we're already in https mode, this wont hurt""
+echo "checking the current URL's. If we're already in https mode, this wont hurt"
 git remote -v
 #
-echo, "First we change the urls"
+echo "First we change the urls"
 git remote set-url origin https:github.com/wa5qjh/Perl.git
 echo " then verify it was set properly.  you should see something like:"
 echo "origin https://github.com/wa5qjh/Perl.git (fetch)"
