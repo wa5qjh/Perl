7cfe3f92ff90c49f284f8d7d7c07ff4e79288bb3
Gary B. Corell <wa5qjh@xmission.com>
2017-04-30 08:14:14 +0800

    new file:   change2https 	new file:   change2ssh.sh

---
+++ change2ssh.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Gary Corell
+# 04/30/17  07:44 AM
+
+echo "Change to ssh mode for github"
+echo, "This will take a few minutes. We are setting up an ssh key on  github"
+ssh -T git@github.com
+#
+echo, " next we change the urls"
+git remote set-url origin git@github.com:wa5qjh/Perl.git
+echo " then verify it was set properly.  you should see something like:"
+echo "origin git@github.com:wa5qjh/Perl.git (fetch)"
+echo "origin git@github.com:wa5qjh/Perl.git (push)"
+git remote -v
+echo "git SHOULD recognize that we are changing modes and act accordingly."
+echo "next we'll save credentials in cache,  tho you may be asked once "
+git config --global credential.helper cache
+
+echo " I think  we're done. just in case tho lets check the urls"
+git remote -v
+
+# just incase it's needed, heres a command to set the upstream urls
+# git push --set-upstream git@github.com:wa5qjh/Perl.git master
+To revert:  git push --set-upstream https://github.com/wa5qjh/Perl.git master
+
